cad3830968bb5b25470fa8ec4b4e3fa201fccacb
ALIZA STONE <amstone326@gmail.com>
2018-08-08 20:54:08 -0700

    update schema to have users and not use AUTOINCREMENT

--- app/schema.sql
+++ app/schema.sql
@@ -1,33 +1,44 @@
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS brands;
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS outfits;
 DROP TABLE IF EXISTS outfit_items;
 
+CREATE TABLE users (
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
 CREATE TABLE brands (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE articles (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
     article_type TEXT NOT NULL,
     brand_id INTEGER NOT NULL,
-    FOREIGN KEY (brand_id) REFERENCES brands (id)
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (brand_id) REFERENCES brands (rowid),
+    FOREIGN KEY (user_id) REFERENCES users (rowid)
 );
 
 CREATE TABLE outfits (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    occasion TEXT NOT NULL
+    occasion TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (rowid)
 );
 
 CREATE TABLE outfit_items (
     outfit_id INTEGER NOT NULL,
     article_id INTEGER NOT NULL,
+    FOREIGN KEY (outfit_id) REFERENCES outfits (rowid),
+    FOREIGN KEY (article_id) REFERENCES articles (rowid),
     PRIMARY KEY (outfit_id, article_id)
 );
 
 
 /*
+The brands table is shared amongst all users
+
 Article types:
 - pant
 - blouse
